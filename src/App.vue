562c24111d43c26e9cb27b3255a0e5acb1b9394b
vakhid <vakha.9991@gmail.com>
2021-09-04 17:53:04 +0300

    Добавить верстку добавления товара

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <header class="header">
+        <div class="header-left">
+          <h1 class="header-title">Добавление товара</h1>
+        </div>
+        <div class="header-right">
+          <button>sad</button>
+        </div>
+      </header>
+      <section class="main">
+        <div class="main__block-left">
+          <AddItem/>
+        </div>
+        <div class="main__block-right">
+          <Item/>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddItem from './components/AddItem.vue'
+import Item from './components/Item.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddItem,
+    Item
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Source Sans Pro;
+  background: #E5E5E5;
+  height: 100%;
+}
+
+.container {
+  padding: 32px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 16px;
+
+  &-title {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 35px;
+    color: #3F3F3F;
+  }
+}
+
+.main {
+  display: flex;
+
+  &__block {
+    &-left {
+      width: 332px;
+    }
+
+    &-right {
+      width: 100%;
+    }
+  }
 }
 </style>
---
+++ src/components/AddItem.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="form">
+    <form action="">
+      <div class="form-group">
+        <div class="form-group__header">
+          <span class="form-group__header-text necessarily">Наименование товара</span>
+        </div>
+        <div class="form-group__body">
+          <input class="form-group__body-input" type="text" placeholder="Введите наименование товара">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="form-group__header">
+          <span class="form-group__header-text">Наименование товара</span>
+        </div>
+        <div class="form-group__body">
+          <textarea class="form-group__body-textarea" placeholder="Введите наименование товара"/>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="form-group__header">
+          <span class="form-group__header-text necessarily">Наименование товара</span>
+        </div>
+        <div class="form-group__body">
+          <input class="form-group__body-input" type="text" placeholder="Введите наименование товара">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="form-group__header">
+          <span class="form-group__header-text necessarily">Наименование товара</span>
+        </div>
+        <div class="form-group__body">
+          <input class="form-group__body-input" type="text" placeholder="Введите наименование товара">
+        </div>
+      </div>
+      <div class="form-group">
+        <button class="form-group__submit">
+          <span class="form-group__submit-text">Добавить товар</span>
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddItem'
+}
+</script>
+
+<style scoped lang="scss">
+.form {
+  background: #FFFEFB;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+  padding: 24px;
+  border-radius: 4px;
+  position: sticky;
+  top: 1px;
+
+  &-group:nth-child(n+2) {
+    margin-top: 16px;
+  }
+
+  &-group {
+
+    &__header {
+      margin-bottom: 4px;
+
+      &-text {
+        font-size: 10px;
+        line-height: 13px;
+        letter-spacing: -0.02em;
+        color: #49485E;
+      }
+    }
+
+    &__body {
+
+      &-input {
+        background: #FFFEFB;
+        border: none;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+        border-radius: 4px;
+        padding: 16px 10px;
+        width: 100%;
+      }
+
+      &-textarea {
+        background: #FFFEFB;
+        border: none;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+        border-radius: 4px;
+        padding: 16px 10px;
+        width: 100%;
+        height: 108px;
+      }
+    }
+
+    &__submit {
+      width: 100%;
+      background: #EEEEEE;
+      border-radius: 10px;
+      border: none;
+      padding: 10px 65px;
+
+      &-text {
+        font-family: Inter;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 12px;
+        line-height: 15px;
+        text-align: center;
+        letter-spacing: -0.02em;
+        color: #B4B4B4;
+      }
+    }
+  }
+}
+
+.necessarily::after {
+  content: '*';
+  color: red;
+}
+</style>
