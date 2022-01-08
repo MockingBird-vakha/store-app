6cb2b1409846c0fa46265b9edd15fe136106b153
vakhid <vakha.9991@gmail.com>
2022-01-08 18:18:58 +0300

    Добавить возможность добавления товара

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    items: []
   },
   mutations: {
+    addItem (state, data) {
+      state.items.push(data)
+    }
   },
   actions: {
+    addItem ({ commit }, data) {
+      commit('addItem', data)
+    }
   },
   modules: {
   }
